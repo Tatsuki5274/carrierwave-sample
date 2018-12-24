7ed2bf47710ee885bece669bd3dfe6041dde0312
Tatsuki5274 <watanabe.tatsuki.infratop@gmail.com>
2018-12-24 08:13:55 +0000

    multiple upload

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
+	# avatar と avatars との間に命名規則の関係は一切ない
 	  mount_uploader :avatar, AvatarUploader
+	  #複数投稿の場合は mount_uploaders と sをつける
+	  mount_uploaders :avatars, AvatarsUploader
 	  mount_uploader :audio, AudioUploader
+	  serialize :avatars, JSON # If you use SQLite, add this line.
+
 end
